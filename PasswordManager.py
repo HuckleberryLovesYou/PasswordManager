4158ea4f8299df84dbd8a05db9970219fd716f5e
Tim Matheis <tim.matheis@outlook.com>
2024-09-19 21:59:57 +0200

    Increased readability by splitting the main function into multiple sub-functions

--- PasswordManager.py
+++ PasswordManager.py
@@ -164,14 +164,146 @@ def main() -> None:
         exit("User ended the program")
 
 
-    cli_args_given: bool = False
-    try:
-        parser = argparse.ArgumentParser(description="Password Manager shortcuts\nCommand example: python PasswordManager.py --master-password password --add --title test --username test --length 8")
+    def handle_mode_selection():
+        while True:
+            if cli_args_given:
+                if args.view_boolean:
+                    mode = "view"
+                elif args.add_boolean:
+                    mode = "add"
+                elif args.index_to_remove is not None:
+                    mode = "remove"
+                else:
+                    print("No mode specified or invalid mode selected.")
+                    quit()
+            else:
+                mode = input("Choose mode [view/add/remove/edit/q to quit]: ").lower()
+
+            if mode == "view":
+                handle_view_mode()
+            elif mode == "add":
+                handle_add_mode()
+            elif mode == "remove":
+                handle_remove_mode()
+            elif mode == "edit":
+                handle_edit_mode()
+            elif mode == "q":
+                encrypt_and_quit()
+            else:
+                print("Invalid mode.")
+
+
+
+    def handle_view_mode():
+        view_dict = view()
+        try:
+            for key in view_dict.keys():
+                print(f"{key}:\t\tTitle: {view_dict[key][0]}\tUsername: {view_dict[key][1]}\tPassword: {view_dict[key][2]}\n")
+        except AttributeError:
+            print("No entries found in the database")
+
+        if cli_args_given:
+            encrypt_and_quit()
+
+
+    def handle_add_mode():
+        if cli_args_given:
+            if args.generate_password_boolean:
+                title = args.title
+                username = args.username
+                password = "G"  # spoof that the password input was "G" to enter password generation, so fewer code changes needed
+            else:
+                title = args.title
+                username = args.username
+                password = args.password
+        else:
+            title = input("Title: ")
+            username = input("Username: ")
+            password = input("Password ['G' to generate]: ")
+        if password == "G":
+            if cli_args_given:
+                configure_password_generation = "n"  # spoof that there is no configuration for password generation wanted
+            else:
+                configure_password_generation = input("Configure password generation? [y/n]: ").lower()
+
+            if configure_password_generation == "y":
+                generate_letters = input("Enable the generation of letters (lowercase and uppercase)? [y/n]: ").lower()
+                generate_numbers = input("Enable the generation of numbers? [y/n]: ").lower()
+                generate_special_characters = input("Enable the generation of special characters? [y/n]: ").lower()
+
+                characters_must_occur_once_bool: bool = False
+                characters_must_occur_once = input(
+                    "Force at least one occurrences of above characters? [y/n]: ").lower()
+                if characters_must_occur_once == "y":
+                    characters_must_occur_once_bool = True
+
+                generate_letters_bool: bool = False
+                generate_numbers_bool: bool = False
+                generate_special_characters_bool: bool = False
+
+                while not generate_letters_bool or not generate_numbers_bool or not generate_special_characters_bool:
+                    if generate_letters == "y":
+                        generate_letters_bool = True
+                    if generate_numbers == "y":
+                        generate_numbers_bool = True
+                    if generate_special_characters == "y":
+                        generate_special_characters_bool = True
+                    break
+
+                generate_password_length = input("Enter password length [4-inf]: ")
+                index, generated_password = add(title=title, username=username, password_length=generate_password_length, letters=generate_letters_bool, numbers=generate_numbers_bool, special=generate_special_characters_bool, characters_occurring_at_least_once=characters_must_occur_once_bool)
+
+            else:
+                if cli_args_given:
+                    generate_password_length = args.length
+                else:
+                    generate_password_length = input("Enter password length [8-inf]: ")
+
+                index, generated_password = add(title=title, username=username, password_length=generate_password_length)
+
+        else:
+            index, generated_password = add(title=title, username=username, password=password)
+
+
+        if generated_password != password:
+            print("Your password is set to ", generated_password)
+        if cli_args_given:
+            encrypt_and_quit()
+
+
+    def handle_remove_mode():
+        if cli_args_given:
+            index_to_remove = str(args.index_to_remove)
+        else:
+            index_to_remove = input("Enter index to delete: ")
+
+        if index_to_remove.isdigit():
+            index_to_remove = int(index_to_remove)
+            remove(index_to_remove)
+        else:
+            print("Please enter a valid number")
+
+        if cli_args_given:
+            encrypt_and_quit()
+
+
+    def handle_edit_mode():
+        index_to_edit = input("Enter index to edit: ")
+        if index_to_edit.isdigit():
+            index_to_edit = int(index_to_edit)
+        else:
+            raise Exception("Please enter a valid index")
+        selected_field = input("Please select the field you want to edit [title/username/password]: ")
+        new_field_value = input("Please enter the new value of the field you want to edit: ")
+        edit(index_to_edit, selected_field, new_field_value)
+
+
+    def handle_cli_args():
+        parser = argparse.ArgumentParser(description="Password Manager shortcuts\nCommand example: python PasswordManager.py --master-password password --add --title test --username test --generate-password --length 8")
         parser.add_argument("-m", "--master-password", required=True, action="store", dest="master_password", type=str, help="Enter Master Password for password database")
         parser.add_argument("-v", "--view", action="store_true", dest="view_boolean", help="Used to set the mode to view and view database [Needs --master-password]", required=False)
         parser.add_argument("-a", "--add", action="store_true", dest="add_boolean", help="Used to set the mode to add and add a new entry from database [Needs --master-password]", required=False)
-        parser.add_argument("-r", "--remove", action="store_true", dest="remove_boolean", help="Used to set the mode to remove and remove a entry from database [Needs --master-password]", required=False)
-        parser.add_argument("-i", "--index", action="store", dest="index", help="Set index in database to delete [Needs --remove]", type=int, required=False)
+        parser.add_argument("-r", "--remove", action="store", dest="index_to_remove", help="Used to set the mode to remove and specify a index to remove [Needs --master-password]", required=False)
         parser.add_argument("-t", "--title", action="store", dest="title", help="Set title of new entry [Needs --add]", type=str, required=False)
         parser.add_argument("-u", "--username", action="store", dest="username", help="Set username of new entry [Needs --add]", type=str, required=False)
         parser.add_argument("-p", "--password", action="store", dest="password", help="Set password of new entry [Needs --add, except --generate-password is specified]", type=str, required=False)
@@ -179,163 +311,67 @@ def main() -> None:
         parser.add_argument("-l", "--length", action="store", dest="length", help="Set the length for password generation for new entry [Needs --generate-password]", type=str, required=False)
         parser.add_argument("-d", "--debug", action="store_true", dest="debug", help="Specify this to enable debug mode [Needs --master-password]", required=False)
 
-        args = parser.parse_args()
 
-        cli_args_given = True
 
+        return parser.parse_args(), True
+
+
+    def handle_database_cryptography():
+        for _ in range(3):
+            if cli_args_given:
+                master_password: str = args.master_password
+            else:
+                master_password: str = input("Enter Master Password ['d' to enable debug]: ").lower()
+            key = PasswordManagerCryptography.convert_master_password_to_key(master_password)
+
+            if cli_args_given:
+                if args.debug:
+                    print("Enabling Debug Mode")
+                    key = PasswordManagerCryptography.convert_master_password_to_key(args.master_password)  # needed because of new password entry
+                    PasswordManagerCryptography.encrypt_database(global_filename, key)
+                    print("Database encrypted")
+                    print("Disabling debug mode")
+            else:
+                if master_password == "d":
+                    print("Enabling Debug Mode")
+                    print("Enter password to encrypt the file with")
+                    master_password = input("Enter Master Password: ")
+                    key = PasswordManagerCryptography.convert_master_password_to_key(master_password)  # needed because of new password entry
+                    PasswordManagerCryptography.encrypt_database(global_filename, key)
+                    print("File encrypted")
+                    print("Disabling debug mode")
+            if PasswordManagerCryptography.decrypt_database(global_filename, key):
+                print("Database decrypted")
+                print("If program is now closed without the use of 'q to quit', the database needs to be repaired in debug mode!")
+                return master_password
+            return master_password
+        else:
+            raise Exception("Error while crypting database")
+
+
+
+
+
+
+
+
+
+    cli_args_given = False
+    try:
+        args, cli_args_given = handle_cli_args()
     except:
         print("No arguments found\nUsing interactive mode instead")
-    while True:
+
+
+    for i in range(3):
         filepath, file_found = get_filepath()
         if file_found:
-            while True:
-                if cli_args_given:
-                    master_password = args.master_password
-                else:
-                    master_password = input("Enter Master Password ['d' to enable debug]: ").lower()
-                key = PasswordManagerCryptography.convert_master_password_to_key(master_password)
-                if cli_args_given:
-                    if args.debug:
-                        print("Enabling Debug Mode")
-                        key = PasswordManagerCryptography.convert_master_password_to_key(args.master_password)  # needed because of new password entry
-                        PasswordManagerCryptography.encrypt_database(global_filename, key)
-                        print("File encrypted")
-                        print("Disabling debug mode")
-                        encrypt_and_quit()
-                else:
-                    if master_password == "d":
-                        print("Enabling Debug Mode")
-                        print("Enter password to encrypt the file with")
-                        master_password = input("Enter Master Password: ")
-                        key = PasswordManagerCryptography.convert_master_password_to_key(master_password) #needed because of new password entry
-                        PasswordManagerCryptography.encrypt_database(global_filename, key)
-                        print("File encrypted")
-                        print("Disabling debug mode")
-                if PasswordManagerCryptography.decrypt_database(global_filename, key):
-                    break
-            print("Database decrypted")
-            if not cli_args_given:
-                print("If program is now closed without the use of 'q to quit', the database needs to be repaired in debug mode!")
-            while True:
-                if cli_args_given:
-                    if args.view_boolean:
-                        mode = "view"
-                    elif args.add_boolean:
-                        mode = "add"
-                    elif args.remove_boolean:
-                        mode = "remove"
-                    else:
-                        print("No mode specified")
-                        quit()
-                else:
-                    mode = input("Choose mode [view/add/remove/edit/q to quit]: ").lower()
+            master_password = handle_database_cryptography()
+            #handles key generation, if debug mode was used in cli mode
+            if cli_args_given and args.debug:
+                encrypt_and_quit()
 
-                if mode == "view":
-                    view_dict = view()
-                    try:
-                        for key in view_dict.keys():
-                            print(f"{key}:\t\tTitle: {view_dict[key][0]}\tUsername: {view_dict[key][1]}\tPassword: {view_dict[key][2]}\n")
-                    except AttributeError:
-                        print("No entries found in the database")
-
-                    if cli_args_given:
-                        encrypt_and_quit()
-                elif mode == "add":
-                    if cli_args_given:
-                        if args.generate_password_boolean:
-                            title = args.title
-                            username = args.username
-                            password = "G" #spoof that the password input was "G" to enter password generation, so fewer code changes needed
-                        else:
-                            title = args.title
-                            username = args.username
-                            password = args.password
-                    else:
-                        title = input("Title: ")
-                        username = input("Username: ")
-                        password = input("Password ['G' to generate]: ")
-                    if password == "G":
-                        if cli_args_given:
-                            configure_password_generation = "n" #spoof that there is no configuration for password generation wanted
-                        else:
-                            configure_password_generation = input("Configure password generation? [y/n]: ").lower()
-
-                        if configure_password_generation == "y":
-                            generate_letters = input("Enable the generation of letters (lowercase and uppercase)? [y/n]: ").lower()
-                            generate_numbers = input("Enable the generation of numbers? [y/n]: ").lower()
-                            generate_special_characters = input("Enable the generation of special characters? [y/n]: ").lower()
-
-                            characters_must_occur_once_bool: bool = False
-                            characters_must_occur_once = input("Force at least one occurrences of above characters? [y/n]: ").lower()
-                            if characters_must_occur_once == "y":
-                                characters_must_occur_once_bool = True
-
-                            generate_letters_bool: bool = False
-                            generate_numbers_bool: bool = False
-                            generate_special_characters_bool: bool = False
-
-                            while not generate_letters_bool or not generate_numbers_bool or not generate_special_characters_bool:
-                                if generate_letters == "y":
-                                    generate_letters_bool = True
-                                if generate_numbers == "y":
-                                    generate_numbers_bool = True
-                                if generate_special_characters == "y":
-                                    generate_special_characters_bool = True
-                                break
-
-                            while True:
-                                generate_password_length = input("Enter password length [8-inf]: ")
-                                index, generated_password = add(title=title, username=username, password_length=generate_password_length, letters=generate_letters_bool, numbers=generate_numbers_bool, special=generate_special_characters_bool, characters_occurring_at_least_once=characters_must_occur_once_bool)
-                                break
-
-                        else:
-                            while True:
-                                if cli_args_given:
-                                    generate_password_length = args.length
-                                else:
-                                    generate_password_length = input("Enter password length [8-inf]: ")
-
-                                index, generated_password = add(title=title, username=username, password_length=generate_password_length)
-                                break
-
-
-                    else:
-                        index, generated_password = add(title=title, username=username, password=password)
-                    if generated_password != password:
-                        print("Your password is set to ", generated_password)
-                    if cli_args_given:
-                        encrypt_and_quit()
-
-                elif mode == "remove":
-                    if cli_args_given:
-                        index_to_remove = str(args.index)
-                    else:
-                        index_to_remove = input("Enter index to delete: ")
-
-                    if index_to_remove.isdigit():
-                        index_to_remove = int(index_to_remove)
-                        remove(index_to_remove)
-                    else:
-                        print("Please enter a valid number")
-
-                    if cli_args_given:
-                        encrypt_and_quit()
-                elif mode == "edit":
-                    index_to_edit = input("Enter index to edit: ")
-                    if index_to_edit.isdigit():
-                        index_to_edit = int(index_to_edit)
-                    else:
-                        raise Exception("Please enter a valid index")
-                    selected_field = input("Please select the field you want to edit [title/username/password]: ")
-                    new_field_value = input("Please enter the new value of the field you want to edit: ")
-                    edit(index_to_edit, selected_field, new_field_value)
-                elif mode == "q":
-                    encrypt_and_quit()
-
-                else:
-                    print("Enter a valid mode")
-                    if cli_args_given:
-                        encrypt_and_quit()
+            handle_mode_selection()
 
 
 
